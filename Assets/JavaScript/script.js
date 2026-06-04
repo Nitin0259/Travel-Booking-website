f15356cba81025c3d9b2c22d6181685c0a099776
Nitin0259 <ns7034501@gmail.com>
2026-06-04 21:14:59 +0530

    Work on dropdown

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,13 +2,31 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         const loader = document.getElementById("loader");
 
-    loader.style.opacity = "0";
+        loader.style.opacity = "0";
 
-    setTimeout(() => {
-        loader.style.display = "none";
-    }, 500)
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500)
     }, 800)
 })
+
+// dropdowns
+document.querySelectorAll(".menus").forEach(menuItem => {
+    const dropdown = menuItem.querySelector(".dropdown-menus")
+
+    menuItem.addEventListener("mouseenter", () => {
+        dropdown.classList.remove("hidden")
+    })
+
+    menuItem.addEventListener("mouseleave", () => {
+        dropdown.classList.add("hidden")
+    })
+
+})
+
+
+
+
 
 
 
