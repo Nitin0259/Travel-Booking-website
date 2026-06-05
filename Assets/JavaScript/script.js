67676f5cc856249c9b33c95e7069aa3650351e44
Nitin0259 <ns7034501@gmail.com>
2026-06-05 21:15:55 +0530

    Header-part

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -24,8 +24,31 @@ document.querySelectorAll(".menus").forEach(menuItem => {
 
 })
 
+// dark-light mode btn
+const btn = document.getElementById("myToggleBtn")
+const toggle = document.getElementById("theme_icon")
 
+btn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark")
 
+    if(document.documentElement.classList.contains("dark")) {
+        toggle.classList.remove("fa-moon", "text-slate-600")
+        toggle.classList.add("fa-sun", "text-yellow-400")
+    }
+    else{
+        toggle.classList.remove("fa-sun", "text-yellow-400")
+        toggle.classList.add("fa-moon", "text-slate-600")
+    }
+})
+
+// Menu-bar
+const menu_btn = document.getElementById("menu-btn")
+const menu_icon = document.getElementById("menu-icon")
+
+menu_btn.addEventListener("click", () => {
+    menu_icon.classList.toggle("fa-bars")
+    menu_icon.classList.toggle("fa-xmark")
+})
 
 
 
